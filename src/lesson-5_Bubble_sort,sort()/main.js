0e726f2e397de70137154057324b4cb6147c3977
maximkuznecovv <maximkuznecovv@gmail.com>
2022-01-26 02:35:01 +0300

    lesson-5 сортировка массивов циклами и !!! ОБЯЗАТЕЛЬНАЯ!!! ТОЧКА С ЗАПЯТОЙ (;) ПЕРЕД ЛИТЕРАЛОМ МАССИВА

--- src/lesson-5_Bubble_sort,sort()/main.js
+++ src/lesson-5_Bubble_sort,sort()/main.js
@@ -1,10 +1,14 @@
-const array = [23, 43, 12, 32, 65, 777, 82, 21, 0, 99]
+//------------------------------------------------------------
+const originalArray = [23, 43, 12, 32, 65, 777, 82, 21, 0, 99]
+const array = [...originalArray]
 
+//------------------------------------------------------------
+// ПУЗЫРЬКОВАЯ СОРТИРОВКА
 for (let j = 0; j < array.length; j++) {
     let sort = true;
     for (let i = 0; i < array.length - j; i++) {
         if (array[i] > array[i + 1]) {
-            sort = false; //todo Вопрос для влада. Почему интерпритатор соединяет обе сторчки если не ставить запятую
+            sort = false; // todo !!! НУЖНО СТАВИТЬ ТОЧКУ С ЗАПЯТОЙ, ЕСЛИ С НОВОЙ СТРОЧКИ НАЧИНАЕТСЯ ЛИТЕРАЛ МАССИВА !!! Вопрос для влада. Почему интерпритатор соединяет обе сторчки если не ставить запятую
             [array[i], array[i + 1]] = [array[i + 1], array[i]]
 
             // let temp = array[i + 1]
@@ -16,8 +20,40 @@ for (let j = 0; j < array.length; j++) {
         break
     }
 }
-
 console.log(array)
+
+//------------------------------------------------------------
+
+const mySort = (arr) => {
+    let resultArr = []
+    do {
+        let minValue = Math.min(...arr)
+        resultArr.push(minValue)
+        arr.splice(arr.indexOf(minValue), 1)
+    } while (arr.length !== 0)
+    console.log(resultArr)
+}
+mySort(array)
+
+//------------------------------------------------------------
+const array1 = [...originalArray]
+let newArray = []
+
+const sort = (array) => {
+    let arrLength = array.length
+    for (let i = 0; i < arrLength && array.length > 0; i++) {
+        let minValue = array.reduce((acc, el) => {
+            return acc < el ? acc : el
+        })
+        newArray.push(minValue)
+        array = array.filter(e => e !== minValue)
+    }
+    return (newArray)
+}
+sort(array1)
+console.log(newArray)
+
+//==================================================================================================
 
 // Array.prototype.sort()
 
@@ -26,8 +62,11 @@ console.log(names.sort())
 
 const numbers = [100, 1000, 90, 2]
 
+//------------------------------------------------------------
+
 const compareFn = (a, b) => a - b
 console.log(numbers.sort(compareFn))
+console.log(numbers.sort(compareFn).reverse())
 
 // function compareFn(a, b) {
 //     if (a <= b) {
@@ -37,40 +76,59 @@ console.log(numbers.sort(compareFn))
 //     }
 // }
 
+//------------------------------------------------------------
+
 let students = [
     {
-        name: "Bob",
+        name: 'Bob',
         age: 22,
         isMarried: true,
         scores: 95
     },
     {
-        name: "Alex",
+        name: 'bob',
+        age: 22,
+        isMarried: true,
+        scores: 95
+    },
+    {
+        name: 'Alex',
         age: 23,
         isMarried: true,
         scores: 89
     },
     {
-        name: "Helge",
+        name: 'alex',
+        age: 23,
+        isMarried: true,
+        scores: 19
+    },
+    {
+        name: 'Helge',
         age: 21,
         isMarried: true,
         scores: 89
     },
     {
-        name: "John",
+        name: 'John',
         age: 19,
         isMarried: false,
-        scores: 121
+        scores: 321
     },
     {
-        name: "Nick",
+        name: 'Nick',
         age: 20,
         isMarried: false,
         scores: 120
     },
 ];
 
+//------------------------------------------------------------
+
 console.log(students.sort((a, b) => a.scores - b.scores))
-console.log(students.sort((a, b) => b.age - a.age))
-console.log(students.sort((a, b) => a.name < b.name ? -1 : 1))
-console.log(students.sort((a) => !a.isMarried ? -1 : 1))
+console.log([...students].sort((a, b) => a.name.toUpperCase() < b.name.toUpperCase() ? -1 : 1))
+console.log([...students].sort((a, b) => a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1))
+console.log([...students].sort((a, b) => a.name < b.name ? -1 : 1))
+console.log([...students].sort((a, b) => a.age - b.age))
+console.log([...students].sort((a) => !a.isMarried ? -1 : 1))
+console.log([...students].sort((a) => a.isMarried ? -1 : 1))
